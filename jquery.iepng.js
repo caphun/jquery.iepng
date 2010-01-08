6ab386eafe940754e220738b231ebc69825372d1
caphun <caphun@pazu2k.gearbox.local>
2010-01-08 17:45:04 +0800

    added sizing and spacer options

--- jquery.iepng.js
+++ jquery.iepng.js
@@ -14,8 +14,10 @@
 
 $.fn.iepng = function(options) {
 
-	options = $.extend({}
-	, options);
+	options = $.extend({
+		sizing: 'crop',
+		spacer: 'http://upload.wikimedia.org/wikipedia/commons/5/52/Spacer.gif'
+	}, options);
 
 	return $.each(this, function() {
 		
@@ -26,9 +28,9 @@ $.fn.iepng = function(options) {
 		if (!$.support.opacity && $.browser.version.substr(0,1) < 7) {
 			// determine which fix to apply for the current element if any
 			if (self.is('*[src]')) {
-				$.iepng.fixAttr.apply(this, arguments);
+				$.iepng.fixAttr.apply(this, [options]);
 			} else if (self.css('background-image')) {
-				$.iepng.fixCss.apply(this, arguments);
+				$.iepng.fixCss.apply(this, [options]);
 			}
 		}
 		
@@ -44,29 +46,26 @@ $.iepng = {
 	},
 	
 	// fix elements with png-source
-	fixAttr: function() {
+	fixAttr: function(o) {
 		var self = $(this), 
 			png = self.attr('src'),
 			w = self.attr('width') || self.width(),
-			h = self.attr('height') || self.height(),
-			sizing = 'crop',
-			spacer = 'http://upload.wikimedia.org/wikipedia/commons/5/52/Spacer.gif';
+			h = self.attr('height') || self.height();
 
 		self
-			.attr({src: spacer, width: w, height: h})
-			.css($.iepng.msfilter(png, sizing));
+			.attr({src: o.spacer, width: w, height: h})
+			.css($.iepng.msfilter(png, o.sizing));
 	},
 
 	// fix css background pngs
-	fixCss: function() {
+	fixCss: function(o) {
 		var self = $(this),
-			png = self.css('background-image').replace(/url\(\u0022([^\)]+)\u0022\)/i, '$1'),
-			sizing = 'crop';
+			png = self.css('background-image').replace(/url\(\u0022([^\)]+)\u0022\)/i, '$1');
 
 		self.css(
 			$.extend({
 				backgroundImage: 'none'
-			}, $.iepng.msfilter(png, sizing))
+			}, $.iepng.msfilter(png, o.sizing))
 		);
 	}
 }
